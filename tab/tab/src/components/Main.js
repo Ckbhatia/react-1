85efc769bca555ba0205ab1b16f176ce861ff0e9
Ckbhatia <chetansain86@gmail.com>
2019-09-17 21:36:25 +0530

    feat: create main component and add navigation to it

---
+++ tab/tab/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+import Navigation from './Navigation'
+
+const Main = () => {
+   
+    return (
+        // Header
+        <main className="main-container">
+            {/* Navigation */}
+            <Navigation />
+        </main>
+    )
+}
+
+export default Main;
