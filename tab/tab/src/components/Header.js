e27fd4bb5d64853ef39d9cb833ca0c291aadd1ff
Ckbhatia <chetansain86@gmail.com>
2019-09-17 19:34:18 +0530

    feat: add Header

---
+++ tab/tab/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header className="main-header-container wrapper">
+            <div className="header-container">
+                <h1 className="header-heading">Peoples Of GOT</h1>
+            </div>
+        </header>
+    )
+}
