7e95e9a95259cad16377ad89e9692bee408cfdbb
Ckbhatia <chetansain86@gmail.com>
2019-09-18 15:06:38 +0530

    feat: add crown emoji in heading

--- tab/tab/src/components/Header.js
+++ tab/tab/src/components/Header.js
@@ -4,7 +4,9 @@ export default function Header() {
     return (
         <header className="main-header-container wrapper">
             <div className="header-container">
-                <h1 className="header-heading">Peoples Of GOT</h1>
+                <h1 className="header-heading">
+                    <span className="emoji" aria-label="img" role="img">👑</span>
+                    Peoples Of GOT</h1>
             </div>
         </header>
     )
