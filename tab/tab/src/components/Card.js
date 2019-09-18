f6f6bf07433e9251d77507f6777fa1214e7102b5
Ckbhatia <chetansain86@gmail.com>
2019-09-18 11:52:34 +0530

    feat: Add card component

---
+++ tab/tab/src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+
+export default function Card(props) {
+    const {name, src, text} = props;
+    // console.log('props', props.data);
+
+    return (
+        // Card section
+        <section className="card-section">
+            <div className="card-container">
+                <div className="card-top">
+                    <img src={src} alt="user image"></img>
+                    <h3 className="user-header">{name}</h3>
+                </div>
+                <div className="card-bottom">
+                    <p className="card-para">{text}</p>
+                </div>
+            </div>
+        </section>
+    )
+}
