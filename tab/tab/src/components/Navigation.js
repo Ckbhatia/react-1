db5f9db7075451bf4f818d8d5b5e51ef5167ac60
Ckbhatia <chetansain86@gmail.com>
2019-09-18 14:47:09 +0530

    feat: add logic in navigation className for active button

--- tab/tab/src/components/Navigation.js
+++ tab/tab/src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 
 export default function Navigation(props) {
-    const { data } = props;
+    const { data, active } = props;
 
     return (
             <section className="navigation-section wrapper">
@@ -12,7 +12,7 @@ export default function Navigation(props) {
                     {/* Map the data and exract family */}
                         {data.map((family, index) => {
                             return <li key={index} className="list-item">
-                                      <button className="nav-btn">{family.name}</button>
+                                      <button className={family.name === active ? 'nav-btn active' : 'nav-btn'}>{family.name}</button>
                                    </li>
                         })}
                     </ul>
